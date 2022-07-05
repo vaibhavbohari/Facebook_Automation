c2cb902c6c331a80fb0851dae5b100c40cf93f23
vaibhavbohari <vaibhavbohari@gmail.com>
2022-07-05 10:55:09 +0530

    initial commit

---
+++ src/main/java/Facebook_Automation/Facebook_Automation/App.java
@@ -0,0 +1,13 @@
+package Facebook_Automation.Facebook_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
